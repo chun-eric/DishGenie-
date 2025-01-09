3580484e644144372be25f8dccd300caf61f2926
Eric Chun <eric.chun88800888@gmail.com>
2025-01-09 15:36:37 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-slate-800 text-3xl font-bold'>hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
